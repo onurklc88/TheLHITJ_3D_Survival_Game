8b8a0a5997be48ebe044f82d3b7ed3922d76d6c1
Onur <onr.k@hotmail.com>
2021-11-21 07:51:57 +0300

    Pistol finally fully ended

--- Scripts/Gun.cs
+++ Scripts/Gun.cs
@@ -1,33 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float reloadTime = 60f;
+    public int maxAmmo = 6;
+    public int magazineSize = 12;
+   
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
-    public GameObject impactEfect;
+    public GameObject impactEfectAnimal;
+    public GameObject impactEffectWorld;
+    public GameObject player;
+    public TextMeshProUGUI ammoInfoText;
 
-    // Update is called once per frame
+    [HideInInspector] public bool canShoot = true;
+    [HideInInspector] public int currentAmmo;
+
+
+    private bool isReloading = false;
+    private Animator pistolAnim;
+
+
+    private void Start()
+    {
+        pistolAnim = player.GetComponent<Animator>();
+        currentAmmo = maxAmmo;
+    }
+
+  
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-
-            Shoot();
-
-
-        }
+        FireInput();
+        pistolReload();
+        ammoInfoText.text = this.currentAmmo + " / " + this.magazineSize;
     }
 
 
-
-    void Shoot()
+    public void FireInput()
     {
 
-        muzzleFlash.Play();
+       
+
+        if (Input.GetMouseButtonDown(0))
+        {
+       
+            if(canShoot == true)
+            {
+                
+                Shoot();
+
+                canShoot = false;
+                StartCoroutine(timeBetweenShots());
+            }
+           
+           
+
+        }
+     
+    }
+
+    
+
+public void Shoot()
+    {
+
+        
+      if(canShoot == true)
+        {
+            currentAmmo--;
+
+            muzzleFlash.Play();
         //define raycast
         RaycastHit hit;
         //using raycast to hit objects
@@ -38,17 +86,120 @@ public class Gun : MonoBehaviour
             
         }
 
-       //fetching AIHealth script
-       AIHealth target = hit.transform.GetComponent<AIHealth>();
+
+
+       //if hit object tag is not equal to animal 
+
+      if(hit.transform.tag != "animal")
+        {
+            GameObject impactObect = Instantiate(impactEffectWorld, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactObect, 2f);
+
+        }
+
+       
+
+
+        //fetching AIHealth script
+        AIHealth target = hit.transform.GetComponent<AIHealth>();
 
         //if AIHealth Script is exist hit the damage
         if( target != null)
         {
 
             target.TakeDamage(damage);
+            GameObject impactGO = Instantiate(impactEfectAnimal, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+
+
+}
+
+
+
+    public void pistolReload()
+    {
+        if (isReloading)
+        {
+            return;
+        }
+
+     
+       
+        if (currentAmmo == 0 && magazineSize == 0)
+        {
+            canShoot = false;
+            pistolAnim.SetBool("pistolReload", false);
+          
+            return;
 
         }
-        //Instantiate(impactEfect, hit.point, Quaternion.LookRotation(hit.normal));
+        else if(currentAmmo > 0 || magazineSize > 0)
+        {
+
+            canShoot = true;
+
+        }
+       if(currentAmmo == 0 && isReloading == false)
+        {
+            StartCoroutine(Reload());
+            return;
+
+        }
+     
+
     }
+
+
+
+   IEnumerator Reload()
+    {
+
+
+        if (magazineSize >= maxAmmo)
+        {
+
+            currentAmmo = maxAmmo;
+            magazineSize = magazineSize - maxAmmo;
+           
+        }
+        else
+        {
+            currentAmmo = magazineSize;
+            magazineSize = 0;
+
+        }
+
+        isReloading = true;
+        pistolAnim.SetBool("pistolReload", true);
+        yield return new WaitForSeconds(reloadTime);
+        pistolAnim.SetBool("pistolReload", false);
+         isReloading = false;
+
+    }
+
+
+
+
+    private IEnumerator timeBetweenShots()
+    {
+       
+        //waiting until
+      
+        
+            pistolAnim.SetTrigger("pistolShooting");
+
+       
+       
+        yield return new WaitForSeconds(0.3f);
+        canShoot = true;
+
+
+
+    }
+
+
+
 
 }
