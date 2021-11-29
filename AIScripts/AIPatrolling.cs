2af79f6d33230ae09ebece550fa77afe42404f61
Onur <onr.k@hotmail.com>
2021-11-29 07:35:40 +0300

    New AI behaviours

--- AIScripts/AIPatrolling.cs
+++ AIScripts/AIPatrolling.cs
@@ -7,12 +7,21 @@ public class AIPatrolling : MonoBehaviour
 {
     
     public Transform[] points;
-    private Animator anim;
+    
     private Transform targetWayPoint;
     int current;
-    public float speed;
+    //movement
+    private float speed;
+    public float runSpeed;
+    public float walkSpeed;
     private float rotationSpeed = 2.0f;
-
+    //animations
+    private Animator anim;
+    public float currentPatrolTime = 0;
+    public float currentBreakTime = 0;
+    public float patrolTime = 10f;
+    public float breakTime = 2f;
+    private int counter = 1;
 
     private void Awake()
     {
@@ -22,6 +31,8 @@ public class AIPatrolling : MonoBehaviour
 
     void Start()
     {
+        currentPatrolTime = patrolTime;
+   
         current = 0;
         var playerAgent = GetComponent<NavMeshAgent>();
 
@@ -37,8 +48,8 @@ public class AIPatrolling : MonoBehaviour
 
         AIPatrol();
         Rotation();
-
-
+        Dead();
+        AIbBehaviour();
 
 
     }
@@ -50,9 +61,6 @@ public class AIPatrolling : MonoBehaviour
         {
             //if AI position is not equal to waypoint,AI move towards to waypoint
             transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
-
-          
-
 
         }
         else
@@ -67,13 +75,74 @@ public class AIPatrolling : MonoBehaviour
 
     }
 
+    public void AIbBehaviour()
+    {
+
+        AIHealth health = GetComponent<AIHealth>();
+
+        if(health.AIHealth1 >= 100 && currentPatrolTime > 0)
+        {
+           
+            currentPatrolTime -= 1 * Time.deltaTime;
+            speed = runSpeed;
+            //animation.SetBool("chase", false);
+            anim.SetFloat("patrolTimer", currentPatrolTime);
+            
+                if (currentPatrolTime <= 0)
+                {
+                speed = walkSpeed;
+                currentBreakTime = breakTime;
+
+                   }
+             }
+
+        if(health.hit == false && currentPatrolTime <= 0)
+        {
+            currentBreakTime -= 1 * Time.deltaTime;
+            anim.SetFloat("breakTimer", currentBreakTime);
+                
+                if(currentBreakTime <= 0)
+                {
+                currentPatrolTime = patrolTime;
+
+            }
+             }
+
+        if (health.hit == true)
+        {
+            
+            if(counter == 1)
+            {
+                counter--;
+                currentPatrolTime = patrolTime * 2;
+            }
+             speed = runSpeed;
+            currentPatrolTime -= 1 * Time.deltaTime;
+            anim.SetFloat("patrolTimer", currentPatrolTime);
+            if (currentPatrolTime <= 0)
+            {
+                Debug.Log("girdi");
+                health.hit = false;
+                speed = walkSpeed;
+                currentBreakTime = breakTime;
+
+            }
+
+        }
+
+
+
+    }
+
+
+
 
     public void Rotation()
     {
 
         float rotationStep = rotationSpeed * Time.deltaTime;
         
-        //
+        
         Vector3 directionToTarget = targetWayPoint.position - transform.position;
         Quaternion rotationToTarget = Quaternion.LookRotation(directionToTarget);
 
@@ -83,7 +152,20 @@ public class AIPatrolling : MonoBehaviour
 
 
     }
+    public void Dead()
+    {
+        
+        AIHealth health = GetComponent<AIHealth>();
+        if(health.AIHealth1 <= 0)
+        {
+            //calling Death animation
+            anim.SetBool("isDead", true);
+            speed = 0;
+            Destroy(gameObject, 1f);
+        }
 
+
+    }
 
 
 
