8c2cfbd6b58ddef59e79af0585857932727b4e64
Onur <onr.k@hotmail.com>
2021-12-14 21:55:19 +0300

    New Inventory system

---
+++ Scripts/Inventory-Craft/HotBar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotBar : MonoBehaviour
+{
+    public GameObject asd;
+
+    private void Start()
+    {
+        //asd.GetComponent<RectTransform>().localScale = new Vector3(1.1f, 1.1f, 1);
+    }
+}
---
+++ Scripts/Inventory-Craft/PickItem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickItem : MonoBehaviour
+{
+    [SerializeField] private Camera cam1;
+    public GameObject Item;
+    RaycastHit hit2;
+
+    public static bool pick;
+    public static GameObject y;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam1.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        if (SelectItem.canPick == true)
+        {
+            if (Physics.Raycast(ray, out hit2))
+            {
+                if (hit2.collider.gameObject.tag == "Select")
+                {
+                    y = hit2.collider.gameObject;
+                    Item = hit2.collider.gameObject;
+                }
+                else
+                {
+                    y = null;
+                    Item = null;
+                }
+            }
+
+
+
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(Item);
+                Item = null;
+                pick = true;
+            }
+        }
+        else
+        {
+            y = null;
+            Item = null;
+        }
+    }
+}
