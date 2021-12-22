7d007735bef89b36bcfa8ace229cc376fbe777c6
Onur <onr.k@hotmail.com>
2021-12-22 04:51:20 +0300

    knockback and blood image fix

--- Scripts/Player/HealthScript.cs
+++ Scripts/Player/HealthScript.cs
@@ -3,46 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class HealthScript : MonoBehaviour
 {
    
     
    [HideInInspector] public float playerHealth;
    [HideInInspector] public float maxHealth = 100f;
+    private float currentHealth;
+    public bool damageTaken;
+    public Image bloodImage;
+    public PlayerMovement movementScript;
+    public float healthPercent;
+    private bool Healing;
 
-    public float thrust = 20f;
- 
-    public bool canHit1 = true;
-    Vector3 currentPosition;
-    Rigidbody rb;
+   
 
     void Start()
     {
        
         playerHealth = maxHealth;
-        rb = GetComponent<Rigidbody>();
-        currentPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-      
+        currentHealth = playerHealth;
+        movementScript = FindObjectOfType<PlayerMovement>();
 
     }
 
     
     void Update()
     {
-       
+        
+        takeDamageKnock();
+        knockBackController();
         Heal();
-       
     }
 
     public void takeDamage(float amount)
     {
-        Vector3 deneme = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-      
-      
-
+       
         if (playerHealth > 0) {
-            
-                playerHealth -= amount;
+
+            currentHealth = playerHealth;
+            playerHealth -= amount;
+          
             
 
         }
@@ -51,9 +53,54 @@ public class HealthScript : MonoBehaviour
 
             Debug.Log("player is dead");
         }
+      
+
+    }
+
+    public void knockBackController()
+    {
+
+
+        if(currentHealth > playerHealth)
+        {
+
+            damageTaken = true;
+        }
+        
+    }
+
+
+    public void knockBack(Vector3 direction)
+    {
+
+        movementScript.KnockBack(direction);
 
 
     }
+
+    public void takeDamageKnock()
+    {
+
+        if (damageTaken == true)
+        {
+
+            
+             StartCoroutine(knockBack());
+           
+            
+
+        }
+
+         
+    }
+    public void UpdateBloodImage()
+    {
+
+      
+            healthPercent = (maxHealth - playerHealth) / 100;
+            bloodImage.color = new Color(255, 0, 0, healthPercent);
+            
+        }
 
     public void Heal()
     {
@@ -63,12 +110,27 @@ public class HealthScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.H))
         {
             Debug.Log("girdi");
-            rb.velocity = currentPosition * thrust;
+            playerHealth += 20;
+            currentHealth = playerHealth;
+            healthPercent = (maxHealth - playerHealth) / 100;
+            bloodImage.color = new Color(255, 0, 0, healthPercent);
+         
+            
 
         }
 
 
 
     }
+    IEnumerator knockBack()
+    {
+        yield return new WaitForSeconds(1.1f);
 
+       Vector3 hitDirection = transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z + 0.2f);
+        
+       hitDirection = hitDirection.normalized;
+        knockBack(hitDirection);
+        damageTaken = false;
+        currentHealth = playerHealth;
+    }
 }
