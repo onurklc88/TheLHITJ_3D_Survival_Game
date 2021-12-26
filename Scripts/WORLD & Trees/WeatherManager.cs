0f972ce9d4808e7bf0840c163e7596c745e52fea
Onur <onr.k@hotmail.com>
2021-12-26 12:43:43 +0300

    Dynamic weather day2

--- Scripts/WORLD & Trees/WeatherManager.cs
+++ Scripts/WORLD & Trees/WeatherManager.cs
@@ -9,22 +9,56 @@ public class WeatherManager : MonoBehaviour
     public enum Weather {NONE, SUNNY, RAIN, SNOW};
     public Weather currentWeather;
     public biome currentBiome;
-    
+
+
+    [Header("Light Settings")]
+    public Light sunLight;
+    public float defaultLightIntensity;
+    public float rainLightIntensity;
+    public float winterLightIntensity;
+
+    private Color defaultLightColor;
+    public Color rainColor;
+    public Color winterColor;
+
+
     //time settings
+    public static float timer;
     public float weatherTime = 10f;
     public float sunnyTime;
-    public float rainTime;
+    public float rainTime = Mathf.FloorToInt(timer / 60f);
     public float snowTime;
+    //fog settings
+    public float endDistance = 60f;
 
     private void Start()
     {
+        endDistance = 60f;
         currentBiome = biome.FIRST_BIOME;
         currentWeather = Weather.SUNNY;
         this.weatherTime = this.sunnyTime;
+        //endDistance = RenderSettings.fogEndDistance;
+        this.defaultLightColor = this.sunLight.color;
+        this.defaultLightIntensity = this.sunLight.intensity;
+       
     }
     private void Update()
     {
+      
         weatherController();
+
+        if(this.currentBiome == biome.THIRD_BIOME)
+        {
+            RenderSettings.fogColor = Color.black;
+            if(endDistance >= 5f)
+            {
+                endDistance -= Time.deltaTime;
+                RenderSettings.fogEndDistance = endDistance;
+            }
+           
+
+        }
+       
     }
 
     public void changeWeather(Weather weatherType)
@@ -52,11 +86,12 @@ public class WeatherManager : MonoBehaviour
      
     public void weatherController()
     {
-       
+        
 
         if (this.currentWeather == Weather.SUNNY)
         {
-            this.sunnyTime -= Time.deltaTime;
+            this.sunnyTime -= Time.deltaTime / 60;
+            aLerpLightColor(this.sunLight, defaultLightColor);
         }
         else
         {
@@ -65,11 +100,14 @@ public class WeatherManager : MonoBehaviour
 
         if (this.currentWeather == Weather.RAIN)
         {
-            this.rainTime -= Time.deltaTime;
+            
+            rainTime -= Time.deltaTime / 60f;
+            aLerpLightColor(this.sunLight, rainColor);
+            
         }
         else
         {
-            rainTime = weatherTime;
+            rainTime = weatherTime * 3f;
         }
 
         if (this.currentBiome == biome.FIRST_BIOME || this.currentBiome == biome.SECOND_BIOME)
@@ -83,14 +121,17 @@ public class WeatherManager : MonoBehaviour
             if (this.rainTime <= 0f)
             {
                 changeWeather(Weather.SUNNY);
-                rainTime = weatherTime;
+                
 
             }
         }
 
+    }
+    private void aLerpLightColor(Light light, Color c)
+    {
+        light.color = Color.Lerp(light.color, c, 0.2f * Time.deltaTime);
 
 
     }
-
 
 }
